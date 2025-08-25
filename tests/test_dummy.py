b3684d530997a3406a6f2c1f76dd89d3514345a2
Valerio Velardo <velardovalerio@gmail.com>
2025-08-25 16:19:03 +0200

    add dummy files to ensure pre commit hooks are successful

---
+++ tests/test_dummy.py
@@ -0,0 +1,10 @@
+"""
+Dummy unit test to ensure that the package is not empty and can be pushed to
+git. Deleted when the package contains actual code.
+"""
+
+from your_library_name.dummy import dummy_function
+
+
+def test_dummy_function():
+    assert dummy_function() == 1
---
+++ your_library_name/dummy.py
@@ -0,0 +1,10 @@
+"""
+A dummy file used to ensure the package is not empty and can be pushed to git
+with the pre-commit hook enabled. To be removed, as soon as the package contains
+actual code.
+"""
+
+
+def dummy_function():
+    """A dummy function that does nothing."""
+    return 1
